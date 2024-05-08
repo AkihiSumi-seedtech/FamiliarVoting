1c6014ce3d9b705891be90f45838b6c587586e46
VC02124 <suzuki.rakuko@M-389.local>
2024-05-08 11:53:47 +0900

    okay

--- src/resources/js/Pages/Admin/ElectionCard.jsx
+++ src/resources/js/Pages/Admin/ElectionCard.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ElectionCard = () => {
   return (
-<div id="main" class="p-2 justify-around ml-32 h-48 w-3/4 flex items-start flex items-center bg-white-200 border-solid border-2 border-bg-slate-500 rounded-lg">
+<button id="main" class="p-2 justify-around ml-32 h-48 w-3/4 flex items-start flex items-center bg-white-200 border-solid border-2 border-bg-slate-500 rounded-lg">
     <div>
-    <div class="border-solid border-2 border-black bg-white  py-4 w-80 flex flex-col">Name</div>
-    <div class="bg-green-400  py-4 w-32 mt-4 flex flex-col flex items-start rounded-lg">Status</div>
+    <div class="border-solid border-2 border-gray bg-white  py-4 w-80 flex flex-col rounded-lg font-bold">Name</div>
+    <div class="bg-green-400 py-4 w-32 mt-4 flex flex-col items-center justify-center rounded-lg font-bold">
+  Status
+</div>
+
     </div>
 
 <div class="flex flex-low">
-    <div class="border-solid border-2 border-black bg-white  py-0.3 w-32  mr-4 text-center">開始日
-    <div class="py-4 w-32 mt-10 flex flex-col">日付</div>
+    <div class="border-solid border-2 border-gray bg-white  py-0.3 w-32  mr-4 pt-5 text-center rounded-lg font-bold">開始日
+    <div class="py-4 w-32 mt-10 flex flex-col font-bold">日付</div>
     </div>
-    <div class="border-solid border-2 border-black bg-white py-0.3 w-32 text-center">終了日
-    <div class="py-4 w-32 mt-10 flex flex-col">日付</div>
+    <div class="border-solid border-2 border-gray bg-white py-0.3 w-32 pt-5 text-center rounded-lg font-bold">終了日
+    <div class="py-4 w-32 mt-10 flex flex-col font-bold">日付</div>
     </div>
     </div>
-</div>
-
+</button>
   )
 }
 
