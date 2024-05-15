243a3c588488bdd8644e0a96c9a1e31f9c09a52f
RakukoS <suzuki.rakuko@M-389.local>
2024-05-15 15:54:48 +0900

    ValidationOK

--- src/resources/js/Pages/Admin/Candidates/index.jsx
+++ src/resources/js/Pages/Admin/Candidates/index.jsx
@@ -2,40 +2,40 @@ import React from 'react'
 import ElectionLayout from '@/Layouts/ElectionLayout'
 import { useForm } from '@inertiajs/react'
 
-
-    const Candidates = () => {
-   
-    const {data, setData, post} =useForm({
+const Candidates = () => {
+    const { data, setData, post } = useForm({
         file: null
     })
-   
-    const handleFileChange= (e) => {
-        
+
+    const handleFileChange = (e) => {
         const file = e.target.files[0];
-        if(file){
+        if (file) {
             setData({ file: file });
         }
     }
 
-    const handleImport = (e) =>{
-        e.preventDefault()
-        
-        post(route('admin.candidates.import'),{
-            onSuccess: () =>{
-                console.log('成功!')
-            }
+    const handleImport = (e) => {
+        e.preventDefault();
+        if (!data.file) {
+            alert('ファイルを選択してください');
+            return;
         }
-        
-        )
+
+        post(route('admin.candidates.import'), {
+            data: { file: data.file },
+            onSuccess: () => {
+                console.log('成功!');
+            }
+        });
     }
 
     return (
         <div>
-            <input id ='file' type='file' name ='file' onChange={handleFileChange} />
-            <button tyoe='submit' value={data.file} onClick={handleImport}>Upload</button>
+            <input id='file' type='file' name='file' onChange={handleFileChange} />
+            <button type='button' onClick={handleImport}>Upload</button>
         </div>
     )
 }
 
-Candidates.layoute = page => <ElectionLayout title="立候補者" children={page} />
+Candidates.layout = page => <ElectionLayout title="立候補者" children={page} />
 export default Candidates
