1a0c307bb730e89caa9b809c0dca697f594cd31b
RakukoS <suzuki.rakuko@M-389.local>
2024-05-17 14:32:45 +0900

    AllOkay

--- src/resources/js/Pages/Admin/Candidates/CandidateCard.jsx
+++ src/resources/js/Pages/Admin/Candidates/CandidateCard.jsx
@@ -14,27 +14,31 @@ const CandidateCard = ({ candidates }) => {
 
     return (
         <div>
+            
+            
+        
             <table>
-                <thead>
-                    <tr>
-                        <th scope="col" className="text-center border-t border-b border-l border-r border-gray-300 rpx-6 py-3 bg-gray-200">
-                            立候補者名
-                        </th>
-                        <th scope="col" className="text-center border-t border-b border-l border-r border-gray-300 px-6 py-3 bg-gray-200">
-                            所属
-                        </th>
-                        <th scope="col" className="text-center border-t border-b border-l border-r border-gray-300 px-6 py-3 bg-gray-200">
-                            出馬選挙名
-                        </th>
-                    </tr>
-                </thead>
+            <thead>
+            <tr>
+                <th scope="col" className="text-center border-t border-b border-l border-r border-gray-300 rpx-6 py-3 bg-gray-200">
+                    立候補者名
+                </th>
+                <th scope="col" className="text-center border-t border-b border-l border-r border-gray-300 px-6 py-3 bg-gray-200">
+                    所属
+                </th>
+                <th scope="col" className="text-center border-t border-b border-l border-r border-gray-300 px-6 py-3 bg-gray-200">
+                    出馬選挙名
+                </th>
+            </tr>
+        </thead>
+                
                 <tbody>
                     {candidates.map(candidate => (
-                        
-                        <tr className='border-l border-r border-b border-gray-300' key={candidate.can_id}>
+                       
+                        <tr className='border-l border-r border-b border-t border-gray-300' key={candidate.can_id}>
                             <td className='text-center border-r border-gray-300'>{candidate.can_name}</td>
                             <td className='text-center border-gray-300'>{candidate.can_party}</td>
-                            <td className='border-l border-gray-300'></td>
+                            <td className='text-center border-l border-gray-300'></td>
                         </tr>
                     ))}
                 </tbody>
